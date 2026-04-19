cdf2610db571b7ce4bfe9590cf5fa7f22e0f96cb
MaxCoppa <maxime.coppa@gmail.com>
2026-04-19 16:55:01 +0000

    update api

--- app/api.py
+++ app/api.py
@@ -38,12 +38,113 @@ def show_welcome_page():
         "Model_version": "0.3",
     }
 
-
-@app.post("/predict", tags=["Predict"])
-async def predict(params: dict) -> dict:
-    """
-    Predict
-    """
+@app.get("/predict", tags=["Predict"])
+async def predict(
+    RET_1: float = None,
+    VOL_YES_1: float = None,
+    VOL_NO_1: float = None,
+    VOL_TOTAL_1: float = None,
+    RET_2: float = None,
+    VOL_YES_2: float = None,
+    VOL_NO_2: float = None,
+    VOL_TOTAL_2: float = None,
+    RET_3: float = None,
+    VOL_YES_3: float = None,
+    VOL_NO_3: float = None,
+    VOL_TOTAL_3: float = None,
+    RET_4: float = None,
+    VOL_YES_4: float = None,
+    VOL_NO_4: float = None,
+    VOL_TOTAL_4: float = None,
+    RET_5: float = None,
+    VOL_YES_5: float = None,
+    VOL_NO_5: float = None,
+    VOL_TOTAL_5: float = None,
+    RET_6: float = None,
+    VOL_YES_6: float = None,
+    VOL_NO_6: float = None,
+    VOL_TOTAL_6: float = None,
+    RET_7: float = None,
+    VOL_YES_7: float = None,
+    VOL_NO_7: float = None,
+    VOL_TOTAL_7: float = None,
+    RET_8: float = None,
+    VOL_YES_8: float = None,
+    VOL_NO_8: float = None,
+    VOL_TOTAL_8: float = None,
+    RET_9: float = None,
+    VOL_YES_9: float = None,
+    VOL_NO_9: float = None,
+    VOL_TOTAL_9: float = None,
+    RET_10: float = None,
+    VOL_YES_10: float = None,
+    VOL_NO_10: float = None,
+    VOL_TOTAL_10: float = None,
+    RET_MEAN_5: float = None,
+    RET_STD_5: float = None,
+    VOL_MEAN_5: float = None,
+    RET_MEAN_10: float = None,
+    RET_STD_10: float = None,
+    VOL_MEAN_10: float = None,
+    DIST_50: float = None,
+    DIST_MIN_20: float = None,
+    DIST_MAX_20: float = None,
+    VOL_IMBALANCE: float = None,
+    ts: str = None,
+) -> dict:
+    params = {
+        "RET_1": RET_1,
+        "VOL_YES_1": VOL_YES_1,
+        "VOL_NO_1": VOL_NO_1,
+        "VOL_TOTAL_1": VOL_TOTAL_1,
+        "RET_2": RET_2,
+        "VOL_YES_2": VOL_YES_2,
+        "VOL_NO_2": VOL_NO_2,
+        "VOL_TOTAL_2": VOL_TOTAL_2,
+        "RET_3": RET_3,
+        "VOL_YES_3": VOL_YES_3,
+        "VOL_NO_3": VOL_NO_3,
+        "VOL_TOTAL_3": VOL_TOTAL_3,
+        "RET_4": RET_4,
+        "VOL_YES_4": VOL_YES_4,
+        "VOL_NO_4": VOL_NO_4,
+        "VOL_TOTAL_4": VOL_TOTAL_4,
+        "RET_5": RET_5,
+        "VOL_YES_5": VOL_YES_5,
+        "VOL_NO_5": VOL_NO_5,
+        "VOL_TOTAL_5": VOL_TOTAL_5,
+        "RET_6": RET_6,
+        "VOL_YES_6": VOL_YES_6,
+        "VOL_NO_6": VOL_NO_6,
+        "VOL_TOTAL_6": VOL_TOTAL_6,
+        "RET_7": RET_7,
+        "VOL_YES_7": VOL_YES_7,
+        "VOL_NO_7": VOL_NO_7,
+        "VOL_TOTAL_7": VOL_TOTAL_7,
+        "RET_8": RET_8,
+        "VOL_YES_8": VOL_YES_8,
+        "VOL_NO_8": VOL_NO_8,
+        "VOL_TOTAL_8": VOL_TOTAL_8,
+        "RET_9": RET_9,
+        "VOL_YES_9": VOL_YES_9,
+        "VOL_NO_9": VOL_NO_9,
+        "VOL_TOTAL_9": VOL_TOTAL_9,
+        "RET_10": RET_10,
+        "VOL_YES_10": VOL_YES_10,
+        "VOL_NO_10": VOL_NO_10,
+        "VOL_TOTAL_10": VOL_TOTAL_10,
+        "RET_MEAN_5": RET_MEAN_5,
+        "RET_STD_5": RET_STD_5,
+        "VOL_MEAN_5": VOL_MEAN_5,
+        "RET_MEAN_10": RET_MEAN_10,
+        "RET_STD_10": RET_STD_10,
+        "VOL_MEAN_10": VOL_MEAN_10,
+        "DIST_50": DIST_50,
+        "DIST_MIN_20": DIST_MIN_20,
+        "DIST_MAX_20": DIST_MAX_20,
+        "VOL_IMBALANCE": VOL_IMBALANCE,
+        "ts": ts,
+    }
 
     X = pd.DataFrame([params])
 
@@ -54,7 +155,6 @@ async def predict(params: dict) -> dict:
 
     prediction = float(model.predict(X)[0])
     return {"prediction": prediction}
-
 
 @app.get("/predict/example", tags=["Predict"])
 async def predict_example():
